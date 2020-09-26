c788e21cb0a33f0d07285c8b7300252da1011deb
Your Name <you@example.com>
2020-09-26 17:34:43 +0300

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,52 @@
+package com.company;
+
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        //Scanner a = new Scanner(System.in);
+        //int c = a.nextInt();
+        //int b = a.nextInt();
+        double z = 5.678;
+       // System.out.println(c + " " + b);
+        //System.out.printf("%.2f",z);
+
+
+        if(z > 0) {
+            z++;
+        } else if(z == 5) {
+
+        } else if(4 == 3) {
+
+        } else {
+
+        }
+
+        char ch = 'P';
+
+        switch (ch) {
+            case 'A':
+                System.out.println("A");
+                break;
+            case 'B':
+                System.out.println("B");
+                break;
+            case 'C':
+                System.out.println("C");
+                break;
+            default:
+                System.out.println("default");
+
+
+        }
+        // тернарный оператор
+        int m = (z > 8.5) ? 100 : 900;
+        System.out.println((z > 8.5) ? 100 : 900);
+
+
+
+
+    }
+}
